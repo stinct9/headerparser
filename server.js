b2618a046ec561b7845524198030e5679797b1a0
J <jay2689@gmail.com>
2017-06-04 16:37:53 +0000

    Finished API

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 var express = require("express");
 var app = express();
 
-app.get('/:data', function(req,res) {
-    var data = req.params.data;
-    console.log(data);
+var getClientAddress = function (req) {
+        return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+        || req.connection.remoteAddress;
+};
+
+
+
+app.get('/whoami', function(req,res) {
     
-    res.end(data);
+    var ip = req.connection.remoteAddress;
+    var client = req.connection.client;
+    
+    var getClientAddress = req.headers['x-forwarded-for'];
+    var userAgent = req.headers['user-agent'].split('(')[1].split(')')[0] ;
+    var lang = req.headers['accept-language'].split(',')[0];
+    
+    console.log(getClientAddress);
+    console.log(userAgent);
+    console.log(lang);
+    
+    var data = {
+        ipaddress : getClientAddress,
+        language : lang,
+        software : userAgent
+    };
+    
+    res.end(JSON.stringify(data));
 })
 
 
 app.listen(8080, function () {
    console.log("App listening on 8080."); 
 });
+
